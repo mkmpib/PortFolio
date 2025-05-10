5ba336662003d471ffe2e29d79849b7079a0f1bd
Mohit Kumar <mohitmpib@gmail.com>
2025-05-10 17:05:36 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//  ========== Typing Animation ==========
+
+var typed = new Typed(".typing", {
+  strings: ["Full Stack Developer", "Frontend Developer", "BackEnd Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
